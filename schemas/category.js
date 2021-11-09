d241654180aa31c87426a471b8b1a90e44b8f493
prim0rdial <jeremy.pro.lp@gmail.com>
2021-11-09 14:22:20 +0100

    init

---
+++ schemas/category.js
@@ -0,0 +1,28 @@
+export default {
+  name: "category",
+  title: "Category",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => [
+        Rule.required()
+          .min(3)
+          .error("A title of min. 10 characters is required"),
+        Rule.max(50).warning("Shorter titles are usually better"),
+      ],
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+};
---
+++ schemas/post.js
@@ -0,0 +1,61 @@
+export default {
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => [
+        Rule.required()
+          .min(10)
+          .error("A title of min. 10 characters is required"),
+        Rule.max(120).warning("Shorter titles are usually better"),
+      ],
+    },
+    {
+      name: "exercpt",
+      title: "exercpt",
+      type: "text",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" } }],
+      validation: (Rule) => [Rule.required(), Rule.unique()],
+    },
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "date",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "body",
+      title: "Body",
+      type: "blockContent",
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+};
